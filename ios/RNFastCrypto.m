d7b115c7148b8002e94503c50c9c59c004f2329b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2019-11-19 16:24:26 +0100

    feat: enable create_blocks_request

--- ios/RNFastCrypto.m
+++ ios/RNFastCrypto.m
@@ -14,22 +14,59 @@
     return dispatch_queue_create("io.exodus.RNFastCrypto.MainQueue", qosAttribute);
 }
 
-RCT_EXPORT_MODULE()
++ (void) handleDownloadAndProcess:(NSString*) method
+                                 :(NSString*) params
+                                 :(RCTPromiseResolveBlock) resolve
+                                 :(RCTPromiseRejectBlock) reject {
+    NSString *addr = @"https://xmr.exodus-prod.io/get_blocks.bin";
+    NSString *body = @"AREBAQEBAgEBEAlibG9ja19pZHMKgEGAFbua6YKhl12n15J3wnBXJ6VolLoPskatqrsfRjLjDHN0YXJ0X2hlaWdodAUU7x0AAAAAAAVwcnVuZQsBC25vX21pbmVyX3R4CwA=";
 
-RCT_REMAP_METHOD(moneroCore,
-                 moneroCore:(NSString* ) methodEnum
-                 jsonParams:(NSString *)jsonParams
-                 resolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
+    NSURL *url = [NSURL URLWithString:addr];
+    NSData *binaryData = [[NSData alloc] initWithBase64EncodedString:body options:NSDataBase64DecodingIgnoreUnknownCharacters];
+
+    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:url];
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setHTTPBody:binaryData];
+
+
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:urlRequest completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+
+        resolve([NSString stringWithFormat:@"Content-Length: %lu", data.length]);
+    }];
+    [task resume];
+}
+
++ (void) handleDefault:(NSString*) method
+                      :(NSString*) params
+                      :(RCTPromiseResolveBlock) resolve
+                      :(RCTPromiseRejectBlock) reject {
     char *pszResult = NULL;
-    fast_crypto_monero_core([methodEnum UTF8String], [jsonParams UTF8String], &pszResult);
+
+    fast_crypto_monero_core([method UTF8String], [params UTF8String], &pszResult);
+
     if (pszResult == NULL) {
         resolve(NULL);
+        return;
     }
+
     NSString *jsonResult = [NSString stringWithUTF8String:pszResult];
     free(pszResult);
     resolve(jsonResult);
 }
-@end
 
+RCT_EXPORT_MODULE()
+
+RCT_REMAP_METHOD(moneroCore, :(NSString*) method
+                 :(NSString*) params
+                 :(RCTPromiseResolveBlock) resolve
+                 :(RCTPromiseRejectBlock) reject)
+{
+    if ([method isEqualToString:@"download_and_process"]) {
+        [RNFastCrypto handleDownloadAndProcess:method :params :resolve :reject];
+    } else {
+        [RNFastCrypto handleDefault:method :params :resolve :reject];
+    }
+}
+@end
--- native-libs/test/main.cpp
+++ native-libs/test/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
 #include <serial_bridge_index.hpp>
+#include <string_tools.h>
+
+// Files to be copied.
+// epee/include/storages - might be not needed once refactored.
+// epee/include/file_io_utils.h
+
+// ObjC
+// create_blocks_request
+// HTTP request
+// serial_bridgeextract_utxos
+
 
 int main() {
-    std::string decoded = serial_bridge::decode_address(
-        "{\"address\":\"REMOVED\",\"nettype_string\":\"MAINNET\"}"
-    );
+    std::string m_body = serial_bridge::create_blocks_request(100);
 
-    std::cout << decoded << std::endl;
+    std::cout << m_body << std::endl;
+
     return 0;
 }
