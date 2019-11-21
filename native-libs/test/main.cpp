2359ed1a47039b9ceffc3725bbbb5059e7475432
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2019-11-21 13:56:33 +0100

    feat: test decoding response

--- native-libs/test/main.cpp
+++ native-libs/test/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <cstdlib>
 #include <serial_bridge_index.hpp>
 #include <string_tools.h>
+#include <storages/portable_storage_template_helper.h>
 
 // Files to be copied.
 // epee/include/storages
@@ -12,8 +15,7 @@
 // HTTP request
 // serial_bridgeextract_utxos
 
-
-int main() {
+void test_encode() {
     size_t length = 0;
     const char *body = serial_bridge::create_blocks_request(100, &length);
 
@@ -22,6 +24,35 @@ int main() {
     }
 
     std::cout << std::endl;
+
+    std::free((void *)body);
+}
+
+void test_decode() {
+    std::ifstream in("test/d.bin", std::ios::binary | std::ios::ate);
+    if (!in) {
+        std::cout << "No file\n";
+        return;
+    }
+
+    int size = in.tellg();
+    in.seekg(0, std::ios::beg);
+
+    char *input = new char[size];
+    in.read(input, size);
+
+    std::string m_body(input, size);
+    delete[] input;
+
+    cryptonote::COMMAND_RPC_GET_BLOCKS_FAST::response resp;
+    epee::serialization::load_t_from_binary(resp, m_body);
+
+    std::cout << "Start height: " << resp.start_height << '\n';
+    std::cout << "Current height: " << resp.current_height << '\n';
+}
+
+int main() {
+    test_decode();
 
     return 0;
 }
