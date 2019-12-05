9a32f97136f16b7dca5cbc5617e38246b9545ec8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2019-12-05 19:26:44 +0100

    feat: add experimental android support

--- android/src/main/java/co/airbitz/fastcrypto/RNFastCryptoModule.java
+++ android/src/main/java/co/airbitz/fastcrypto/RNFastCryptoModule.java
@@ -10,6 +10,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.ByteBuffer;
+
 
 public class RNFastCryptoModule extends ReactContextBaseJavaModule {
 
@@ -19,6 +25,8 @@ public class RNFastCryptoModule extends ReactContextBaseJavaModule {
         System.loadLibrary("crypto_bridge"); //this loads the library when the class is loaded
     }
     public native String moneroCoreJNI(String method, String jsonParams);
+    public native int moneroCoreCreateRequest(ByteBuffer requestBuffer);
+    public native String extractUtxosFromBlocksResponse(ByteBuffer buffer, String jsonParams);
 
     private final ReactApplicationContext reactContext;
 
@@ -40,6 +48,47 @@ public class RNFastCryptoModule extends ReactContextBaseJavaModule {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
+                if (method.equals("download_and_process")) {
+                    ByteBuffer requestBuffer = ByteBuffer.allocateDirect(1000);
+                    int requestLength = moneroCoreCreateRequest(requestBuffer);
+
+                    try {
+                        URL url = new URL("https://xmr.exodus-prod.io/get_blocks.bin");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("POST");
+                        connection.setRequestProperty("Content-Type", "application/octet-stream");
+                        connection.setDoInput(true);
+                        connection.setDoOutput(true);
+
+                        OutputStream outputStream = connection.getOutputStream();
+                        for (int i = 0; i < requestLength; i++) {
+                            outputStream.write(requestBuffer.get(i));
+                        }
+
+                        outputStream.close();
+                        connection.connect();
+
+                        String contentLength = connection.getHeaderField("Content-Length");
+                        int responseLength = Integer.parseInt(contentLength);
+
+                        InputStream inputStream = connection.getInputStream();
+
+                        byte[] bytes = new byte[responseLength];
+                        inputStream.read(bytes, 0, responseLength);
+
+                        ByteBuffer responseBuffer = ByteBuffer.allocateDirect(responseLength);
+                        responseBuffer.put(bytes, 0, responseLength);
+
+                        String out = extractUtxosFromBlocksResponse(responseBuffer, jsonParams);
+
+                        promise.resolve(out);
+                        return;
+                    } catch(Exception ex) {
+                        promise.resolve(ex.toString());
+                        return;
+                    }
+                }
+
                 try {
                     String reply = moneroCoreJNI(method, jsonParams); // test response from JNI
                     promise.resolve(reply);
